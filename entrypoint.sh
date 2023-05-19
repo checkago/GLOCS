79c3942359ce06ab1c2e774be7d9e8edb7b8dbda
Eugene Checkoy <mfalcon@mail.ru>
2023-05-19 15:50:19 +0300

    new models

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 python manage.py migrate --run-syncdb
 #python manage.py collectstatic --no-input
 
-gunicorn GLOCS.wsgi:application --bind 0.0.0.0:8000 --reload  -w 4
+gunicorn GLOCS.wsgi:application --bind 0.0.0.0:8080 --reload  -w 4
