a8c1d84b90682b5ea6c61357940539a53342272e
Eugene Checkoy <mfalcon@mail.ru>
2023-08-15 13:35:23 +0300

    баннер

--- web/models.py
+++ web/models.py
@@ -1,5 +1,3 @@
-from typing import Type
-
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
